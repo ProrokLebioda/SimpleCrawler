a92416145fd33d857aa051a909a9ceb5aba8f3c9
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-03-10 10:16:59 +0100

    Added simple aggro and aggro lose timer for enemy to stop chasing player when he leaves notice area

--- Enemies/simple_enemy.gd
+++ Enemies/simple_enemy.gd
@@ -1,18 +1,17 @@
 extends CharacterBody2D
-enum ENEMY_STATE {IDLE, WALK}
-
-var active : bool = false
+enum ENEMY_STATE {IDLE, WALK, AGGRO}
 
 @export var move_speed : float = 30
 @export var idle_time : float = 2
 @export var walk_time : float = 2
+@export var aggro_lose_time : float = 4
 
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var sprite = $Sprite2D
-@onready var timer = $Timer
-
+@onready var idle_walk_timer = $IdleWalkTimer
+@onready var aggro_lose_timer = $AggroLoseTimer
 
 var move_direction : Vector2 = Vector2.ZERO
 var current_state : ENEMY_STATE = ENEMY_STATE.IDLE
@@ -21,48 +20,70 @@ func _ready():
 	pick_new_state()
 	
 func _physics_process(delta):
-	if (active):
+	if (current_state == ENEMY_STATE.AGGRO):
 		move_direction = (Globals.player_pos - position).normalized()
-	if (current_state == ENEMY_STATE.WALK):
+		flip_sprite_direction(move_direction)
+		
+	if (current_state != ENEMY_STATE.IDLE):
 		velocity = move_direction * move_speed
 		move_and_slide()
 		
-
-
-func _on_notice_area_body_entered(body):
-	print("Body entered ",body.name )
-	active = true
-	if (current_state != ENEMY_STATE.WALK):
-		#current_state = ENEMY_STATE.WALK
-		pick_new_state()
-	
 	
 func select_new_direction():
 	move_direction = Vector2(
 		randi_range(-1,1),
 		randi_range(-1,1)
 	)
+	flip_sprite_direction(move_direction)
 	
+
+func flip_sprite_direction(move_direction : Vector2):
 	if (move_direction.x < 0):
 		sprite.flip_h = true
 	elif(move_direction.x > 0):
 		sprite.flip_h = false
 
-
 func pick_new_state():
-	if (current_state == ENEMY_STATE.IDLE):
-		state_machine.travel("chicken_walk_right")
-		print("ToWalk")
-		current_state = ENEMY_STATE.WALK
-		select_new_direction()
-		timer.start(walk_time)
-	elif(current_state == ENEMY_STATE.WALK):
-		state_machine.travel("chicken_idle_right")
-		print("ToIdle")
-		current_state = ENEMY_STATE.IDLE
-		timer.start(idle_time)
+	match current_state:
+		ENEMY_STATE.IDLE:
+			state_machine.travel("chicken_walk_right")
+			print("ToWalk")
+			current_state = ENEMY_STATE.WALK
+			select_new_direction()
+			idle_walk_timer.start(walk_time)
+		
+		ENEMY_STATE.WALK:
+			state_machine.travel("chicken_idle_right")
+			print("ToIdle")
+			current_state = ENEMY_STATE.IDLE
+			idle_walk_timer.start(idle_time)
+		
+		ENEMY_STATE.AGGRO:				
+			state_machine.travel("chicken_idle_right")
+			current_state = ENEMY_STATE.IDLE
+			idle_walk_timer.start(idle_time)
+			
 
+func _on_notice_area_body_entered(body):
+	print("Body entered ",body.name )
+	if (idle_walk_timer.time_left > 0):
+		idle_walk_timer.stop()
+		
+	if (aggro_lose_timer.time_left > 0):
+		aggro_lose_timer.stop()
 
-func _on_timer_timeout():
-	if (!active):
+	current_state = ENEMY_STATE.AGGRO
+
+func _on_notice_area_body_exited(body):
+	if (current_state == ENEMY_STATE.AGGRO):
+		aggro_lose_timer.start(aggro_lose_time)
+		print ("Aggro lose timer started")
+		
+func _on_idle_walk_timer_timeout():
+	if (current_state != ENEMY_STATE.AGGRO):
 		pick_new_state()
+
+
+func _on_aggro_lose_timer_timeout():
+	pick_new_state()
+	print ("Aggro lost")
