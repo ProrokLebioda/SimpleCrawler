18191b854b47822672609f2310bb8ff29a5b7486
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-04-07 18:07:43 +0200

    Small change to move code to function for generation of level

--- Globals/levels.gd
+++ Globals/levels.gd
@@ -5,6 +5,13 @@ extends Node
 var rooms = {}
 
 func _ready():
+	generate_random_dungeon()
+	
+func clear_rooms_visited_state():
+	for room in rooms:
+		rooms[room]["is_visited"] = false
+	
+func generate_random_dungeon():
 	# Define a couple of Vector2 keys.
 	var key1 = Vector2(0,0)
 	var key2 = Vector2(0,1)
@@ -18,16 +25,3 @@ func _ready():
 	rooms[key3] = Room_struct.new().create_room(false, Room_struct.ROOM_TYPE.TREASUE)
 	rooms[key4] = Room_struct.new().create_room(false, Room_struct.ROOM_TYPE.SHOP)
 	rooms[key5] = Room_struct.new().create_room(false, Room_struct.ROOM_TYPE.BOSS)
-
-	# Example: Accessing a room's data
-	var room = rooms[key1]
-	print("Room at: ", key1, ", visited?: ", room["is_visited"], ", Type: ", room["type"])
-	room = rooms[key2]
-	print("Room at: ", key2, ", visited?: ", room["is_visited"])
-	room = rooms[key3]
-	print("Room at: ", key3, ", visited?: ", room["is_visited"])
-
-func clear_rooms_visited_state():
-	for room in rooms:
-		rooms[room]["is_visited"] = false
-	
