c4a7611b08d9e3e8e98992a85729f51f06b99010
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-04-18 17:51:23 +0200

    Changed how starting_room is referenced (passed string) now it uses Room.starting_room to keep it clean

--- Levels/boss_room.gd
+++ Levels/boss_room.gd
@@ -36,30 +36,32 @@ func room_cleared():
 
 	
 func _on_enemy_died(death_position):
-	enemies_left_count-= 1
-	print("Boss died! Remaining Boss count: ", enemies_left_count)
-	if (enemies_left_count <= 0):
-		if get_tree() != null:
+	if get_tree() != null:
+		enemies_left_count-= 1
+		print("Boss died! Remaining Boss count: ", enemies_left_count)
+		if (enemies_left_count <= 0):
 			room_cleared()
-		
-		#if player cleared last boss, end game
-		# TODO_CHANGE: Change back to checking last available level 
-		if Globals.player_at_level == Levels.levels:
-			get_tree().change_scene_to_file("res://UI/game_finished.tscn")
-		else:
-			spawn_ladder(death_position)
+			
+			#if player cleared last boss, end game
+			# TODO_CHANGE: Change back to checking last available level 
+			if Globals.player_at_level == Levels.levels:
+				get_tree().change_scene_to_file("res://UI/game_finished.tscn")
+			else:
+				spawn_ladder(death_position)
 
 
 func spawn_ladder(spawn_position):
-	if enemies_left_count <= 0:
-		var ladder = ladder_scene.instantiate()
-		ladder.connect("player_entered_ladder", _on_player_entered_ladder)
-		ladder.global_position = spawn_position
-		objects_node.add_child(ladder)
+	if get_tree() != null:
+		if enemies_left_count <= 0:
+			var ladder = ladder_scene.instantiate()
+			ladder.connect("player_entered_ladder", _on_player_entered_ladder)
+			ladder.global_position = spawn_position
+			objects_node.add_child(ladder)
 		
 func _on_player_entered_ladder():
 	print("Change levels")
 	Globals.player_entered = Globals.Entrance.CENTER
-	update_player_room(Vector3(0,0,Globals.player_at_level+1))
+	var new_player_room = Vector3i(0,0,Globals.player_at_level+1)
+	update_player_room(new_player_room)
 	Levels.rooms[room_vector_position]["is_visited"] = true
-	get_tree().change_scene_to_file("res://Levels/starting_room.tscn")
+	get_tree().change_scene_to_file(Levels.rooms[new_player_room]["scene"])
