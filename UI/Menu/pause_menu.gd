5376b97d8b4bacd8594daa4025d57b735e91ad74
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-04-28 17:30:26 +0200

    Continued work, keyboard doesn't unpause

--- UI/Menu/pause_menu.gd
+++ UI/Menu/pause_menu.gd
@@ -1,15 +1,11 @@
 extends CanvasLayer
 @onready var settings_menu = $settings_menu
 @onready var pause_menu = $PauseMenuLayout
-
+@onready var level = $"../.."
+signal unpause
 func _input(event):
 	if event.is_action_pressed("escape"):
-		_on_pause()
-
-func _on_pause():
-	get_tree().paused = !get_tree().paused
-	visible = !visible
-
+		_on_pause()	
 
 func _on_back_button_pressed():
 	settings_menu.visible = !settings_menu.visible
@@ -17,3 +13,12 @@ func _on_back_button_pressed():
 func toggle_settings():
 	settings_menu.visible = !settings_menu.visible
 	pause_menu.visible = !pause_menu.visible 
+
+
+func _on_settings_button_pressed():
+	toggle_settings()
+
+
+func _on_pause():
+	unpause.emit()
+	visible = !visible
