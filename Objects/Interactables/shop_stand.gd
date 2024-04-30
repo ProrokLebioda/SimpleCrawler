6aadea98011683c09f0db96d57b08b6d469a24f1
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-04-30 17:30:57 +0200

    Added label display for cost of powerup

--- Objects/Interactables/shop_stand.gd
+++ Objects/Interactables/shop_stand.gd
@@ -1,17 +1,22 @@
 extends ItemStand
 
-var health_pickup_scene: PackedScene = preload("res://Objects/Item_Pickups/health_pickup.tscn")
 @onready var collision_shape_2d = $CollisionShape2D
-@export var item_price : int = 3
 @onready var item_spot = $ItemSpot
+@onready var item_price_text = $ItemPriceText
+
+@export var health_pickup_scene: PackedScene = preload("res://Objects/Item_Pickups/health_pickup.tscn")
+@export var item_price : int = 3
 
 func _ready():
 	if is_used: 
 		collision_shape_2d.set_deferred("disabled", true)
+		item_price_text.visible = false
 	else:
 		var item = health_pickup_scene.instantiate()
 		item.position = item_spot.position
 		item_spot.add_child(item)
+		item_price_text.visible = true
+		item_price_text.set_text(str(item_price))
 
 func _on_stand_pay_area_body_entered(body):
 	print(body.name, " entered to pay")
@@ -20,3 +25,4 @@ func _on_stand_pay_area_body_entered(body):
 			Globals.coins -= item_price
 			collision_shape_2d.set_deferred("disabled", true)
 			is_used = true
+			item_price_text.visible = false
