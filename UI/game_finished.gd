7df18d68f932be8cdc3ea3b8396703c0e94e13be
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-05-09 23:09:32 +0200

    Added win and lose sound Added export settings temporarily

--- UI/game_finished.gd
+++ UI/game_finished.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
-
-
+#change to something more fitting than robot
+@export var victory_music: AudioStream
+func _ready():
+	AudioPlayer._play_music(victory_music, -12.0)
+	
+	
 func _on_retry_button_pressed():
 	Globals.reset_player_stats()
 	get_tree().change_scene_to_file(Room.starting_room)
