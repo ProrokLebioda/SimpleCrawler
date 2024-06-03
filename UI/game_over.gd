31465f98b78d9795ba9e9adeedbb082a21de0129
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-06-03 16:43:18 +0200

    Added focus on Retry button in GameOver screen

--- UI/game_over.gd
+++ UI/game_over.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 @export var death_music: AudioStream
+@onready var retry_button = $VBoxContainer/RetryButton
+
 
 func _ready():
 	AudioPlayer._play_music(death_music, -12.0)
-
+	retry_button.grab_focus()
 
 func _on_retry_button_pressed():
 	Globals.reset_player_stats()
@@ -14,3 +16,8 @@ func _on_retry_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_main_menu_button_pressed():
+	AudioPlayer._stop_music()
+	get_tree().change_scene_to_file("res://UI/Menu/main_menu.tscn")
